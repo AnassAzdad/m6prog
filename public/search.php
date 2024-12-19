e36abd6297152b48a0d39535363c76476ce961e8
Gaming Anass <anassazdad3@gmail.com>
2024-12-19 12:48:17 +0100

    (hi)

--- public/search.php
+++ public/search.php
@@ -1,4 +1,11 @@
 <?php
+echo '<pre>';
+if (isset($_GET['search'])) {
+    var_dump($_GET['search']); 
+} else {
+    echo "The 'zoeken' parameter is not set in the URL."; 
+}
+echo '</pre>';
 
 ?>
 
