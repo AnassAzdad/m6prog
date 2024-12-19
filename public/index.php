d5ca6b66680a475da7c6ee665033170c640b04a5
Gaming Anass <anassazdad3@gmail.com>
2024-12-19 12:36:22 +0100

    (hi)

--- public/index.php
+++ public/index.php
@@ -1,11 +1,12 @@
 <?php
+
 require_once '../source/config.php';
 require_once SOURCE_ROOT . 'database.php';
 $connection = database_connect();
 $plaats = 'amsterdam';
-$sql = 'SELECT * FROM weersomstandighedenPerDag WHERE Plaats=? ORDER BY Datum'; 
+$sql = 'SELECT * FROM weeromstandighedenPerDag WHERE Plaats=? ORDER BY Datum'; 
 $stmt = $connection->prepare($sql);
-$stmt->bind_param('s', 'amsterdam');
+$stmt->bind_param('s', $plaats);
 $stmt->execute();
 $result = $stmt->get_result();
 $weersomstandigheden = mysqli_fetch_assoc($result);
--- weer.sql
+++ weer.sql
@@ -2,8 +2,6 @@ CREATE SCHEMA IF NOT EXISTS weer;
 USE weer;
 CREATE TABLE IF NOT EXISTS weeromstandighedenPerDag
 (
-
-);
 idWeeromstandighedenPerDag INT UNSIGNED NOT NULL AUTO_INCREMENT,
 datum DATE NOT NULL,
 aantalGraden DECIMAL NOT NULL,
@@ -12,3 +10,4 @@ regenInMilimeters DECIMAL NOT NULL,
 plaats VARCHAR(120) NOT NULL,
 PRIMARY KEY (idWeeromstandighedenPerDag),
 UNIQUE INDEX idWeeromstandighedenPerDag_UNIQUE (idWeeromstandighedenPerDag ASC) VISIBLE 
+);
